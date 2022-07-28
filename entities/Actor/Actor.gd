f5e2ed34e773bb778550306e2f131179718e75e0
Xinginn <46302636+Xinginn@users.noreply.github.com>
2022-07-28 14:32:42 +0200

    class Player extends Actor
    +extension la class Actor en Player et Npc, afin de separer les réaction aux input (Player) et IA (Npc)

--- entities/Actor/Actor.gd
+++ entities/Actor/Actor.gd
@@ -251,20 +251,6 @@ func _physics_process(delta):
   self.stamina += delta * BASE_STAMINA_REGEN 
   self.mana += delta * BASE_MANA_REGEN
   
-  if is_player:
-    # movement clavier pour player
-    var key_target: Vector2 = Vector2(0.0, 0.0)
-    if Input.is_action_pressed("ui_left"):
-      key_target.x -= 10.0
-    if Input.is_action_pressed("ui_right"):
-      key_target.x += 10.0
-    if Input.is_action_pressed("ui_up"):
-      key_target.y -= 10.0
-    if Input.is_action_pressed("ui_down"):
-      key_target.y += 10.0
-    if key_target != Vector2(0.0, 0.0):
-      move_to(global_position + key_target)
-  
   if target_position != null:
     var final_move_speed = clamp(move_speed - weight_speed_malus, 0, MAX_MOVE_SPEED)
     velocity = global_position.direction_to(target_position) * final_move_speed
