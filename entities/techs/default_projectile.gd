a4c621e5746cc309cfb09f767c6595e46dc36420
Xinginn <46302636+Xinginn@users.noreply.github.com>
2022-09-21 22:46:52 +0200

    ajout tech projectile
    +affinage de la classe ProjectileData pour avoir la velocity et lifespan
    +gestion input du player (temporairement sur '1') pour lancement
    +lancement, orientation et déplacement ok
    +gestion de collision avec un autre actor, perte de PV et suppression de l'instance
    +ajout d'un asset temporaire et creation d'un spriteframe
    -manque toujours blocage de lancement si ressources insuffisantes
    -manque gestion de cooldown
    -manque gestion gain d'xp
    -manque gestion augmentation d'effet selon compétence du lanceur

--- entities/techs/default_projectile.gd
+++ entities/techs/default_projectile.gd
@@ -3,17 +3,22 @@ extends AnimatedSprite
 var caster: Actor
 var tech_data: ProjectileData
 
-var normalized_direction: Vector2
+var is_disabled: bool = false
+var normalized_direction: Vector2 = Vector2(0.0, 0.0)
 var elapsed_time: float = 0
 
- #func _on_animation_finished():
- #  queue_free()
 
-#func _on_body_entered(body):
-#  if body != caster:
-#    if body is Actor:
-#      body.health -= 5.0
+func die() -> void:
+  is_disabled = true
+  queue_free() 
 
+func _on_body_entered(body):
+  if is_disabled: 
+    return
+  if body != caster:
+    if body is Actor:
+      body.health -= 5.0
+      die()
 
 func launch(_caster: Actor) -> void:
   caster = _caster
@@ -26,8 +31,10 @@ func launch(_caster: Actor) -> void:
   caster.mana -= tech_data.cost["mana"]
   
 func _process(delta):
-  global_position += normalized_direction * tech_data.velocity * delta
   elapsed_time += delta
   if elapsed_time >= tech_data.lifespan:
-    queue_free()
+    die()
+  if not is_disabled:
+    global_position += normalized_direction * tech_data.velocity * delta
+
   
--- globals/data.gd
+++ globals/data.gd
@@ -43,7 +43,7 @@ func _ready():
   # id, _name, type, skill ('weapon' pour déduire de l'arme equipée, _xp_gain, _cost ( _range ou area)
   techs.append(StrikeData.new(0, "weapon_strike", "strike", "weapon", 4, {"stamina": 1}, 1.0, 60))
   # _id, _name, _type, _skill, _xp_gain, _cost, _cooldown, _velocity, _lifespan
-  techs.append(ProjectileData.new(1, "default_projectile", "projectile", "armorsmith", 4, {"stamina": 1}, 1.0, 200, 1))
+  techs.append(ProjectileData.new(1, "default_projectile", "projectile", "armorsmith", 4, {"stamina": 1}, 1.0, 200, 20))
   
   
   races = [
