f0a4b13f2ec0a78e4c1f08e17b2fcab3b59b4c29
Félix Ravella <felix.ravella@gmail.com>
2022-09-27 13:33:06 +0200

    wip interface refine 2

---
+++ scenes/playground/refine_panel.gd
@@ -0,0 +1,13 @@
+extends Panel
+
+onready var recipe_container: VBoxContainer = $RefineRecipeContainer
+
+
+func _on_current_refine_changed(data) -> void:
+  pass
+
+func _on_refine_progress_changed(value) -> void:
+  pass
+
+func _ready():
+  pass # Replace with function body.
